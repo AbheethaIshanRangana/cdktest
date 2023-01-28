8958c553cdce175826615b5712d43eb31003cb27
Abheetha Ishan <d.m.abheethaishan@gmail.com>
2023-01-28 21:11:21 +0530

    Add ecs, iam, task and security group

--- infra/lib/infra-stack.ts
+++ infra/lib/infra-stack.ts
@@ -5,6 +5,7 @@ import * as ecsp from 'aws-cdk-lib/aws-ecs-patterns';
 import * as ec2 from 'aws-cdk-lib/aws-ec2';
 import * as elb from 'aws-cdk-lib/aws-elasticloadbalancingv2';
 import { CfnParameter } from 'aws-cdk-lib';
+import * as iam from 'aws-cdk-lib/aws-iam';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class InfraStack extends cdk.Stack {
@@ -75,6 +76,87 @@ export class InfraStack extends cdk.Stack {
     );
 
     alb.addSecurityGroup(albsg);
+
+    // ECS cluster
+    const cluster = new ecs.Cluster(this, `appcluster-${StackEnv}`, {
+      clusterName: `cluster-${StackEnv}`,
+      vpc: vpc
+    });
+
+    // iam role for the task and container
+    const taskrole = new iam.Role(this, `taskrole-${StackEnv}`, {
+      assumedBy: new iam.ServicePrincipal("ecs-tasks.amazonaws.com"),
+      roleName: `task-role-${StackEnv}`,
+      description: "Role for the tasks and containers"
+    });
+
+    taskrole.attachInlinePolicy(
+      new iam.Policy(this, `taskpolicy-${StackEnv}`, {
+        statements: [
+          new iam.PolicyStatement({
+            effect: iam.Effect.ALLOW,
+            actions: ["SES:*"],
+            resources: ["*"]
+          })
+        ]
+      })
+    );
+
+    // task definition
+    const taskdefinition = new ecs.TaskDefinition(this, `task-${StackEnv}`, {
+      family: "task",
+      compatibility: ecs.Compatibility.EC2_AND_FARGATE,
+      cpu: "256",
+      memoryMiB: "512",
+      networkMode: ecs.NetworkMode.AWS_VPC,
+      taskRole: taskrole
+    });
+
+    const container = taskdefinition.addContainer("container", {
+      image: ecs.ContainerImage.fromRegistry("cdkapp"),
+      memoryLimitMiB: 512,
+      logging: ecs.LogDriver.awsLogs({ streamPrefix: `app-logs-${StackEnv}` })
+    });
+
+    container.addPortMappings({ containerPort: 8000 });
+
+    // container security group
+    const ecssg = new ec2.SecurityGroup(this, `ecssg-${StackEnv}`, {
+      vpc: vpc,
+      allowAllOutbound: true
+    });
+
+    ecssg.connections.allowFrom(
+      albsg, 
+      ec2.Port.allTcp(),
+      "Application Load Balancer"
+    );
+
+
+    // ECS service
+    const ecsservice = new ecs.FargateService(this, `app-ecs-service-${StackEnv}`, {
+      cluster: cluster,
+      desiredCount: 2,
+      taskDefinition: taskdefinition,
+      securityGroups: [ecssg],
+      assignPublicIp: true
+    });
+
+    ecsservice.attachToApplicationTargetGroup(targetGroupHttp);
+
+    // Autoscaling based on Memory and CPU usage
+    const scalabletarget = ecsservice.autoScaleTaskCount({
+      minCapacity: 2,
+      maxCapacity: 5
+    });
+
+    scalabletarget.scaleOnMemoryUtilization(`ScaleUpMem-${StackEnv}`, {
+      targetUtilizationPercent: 75
+    });
+
+    scalabletarget.scaleOnCpuUtilization(`ScaleUpCPU-${StackEnv}`, {
+      targetUtilizationPercent: 75
+    });
 
   }
 }
